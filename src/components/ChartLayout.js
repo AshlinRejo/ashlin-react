009ea735561f8094d65ab0674fde79298359fbe7
ashlin <ashlin@flycart.org>
2023-10-11 00:18:34 +0530

    - Removed unwanted code

--- src/components/ChartLayout.js
+++ src/components/ChartLayout.js
@@ -37,7 +37,7 @@ const ChartLayout = () => {
 	const options = {
 		responsive: true,
 	};
-	// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
 	const data = {
 		labels,
 		datasets: [
